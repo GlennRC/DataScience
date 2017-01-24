29a48ff0d3160aa3b21cf39f178c56654da9f7fe
GlennRC <glennrcontreras@gmail.com>
2017-01-23 18:19:56 -0800

    initial commit

---
+++ Lab1/tweet_sentiment.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+import sys
+import threading
+import Queue
+import time
+import socket
+import httplib
+
+from tweepy.auth import OAuthHandler
+from tweepy import Stream
+from tweepy.streaming import StreamListener
+from tweepy.utils import import_simplejson
+import staticconf
+json = import_simplejson()
+
+def  main():
+	print "hello"
+
+if __name__ == '__main__':
+    	main()
